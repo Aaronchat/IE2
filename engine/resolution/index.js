af4ca7e8dd150cbf1a472a313b6a7981dd20545d
Aaronchat <pughenbohm@yahoo.com>
2026-08-18 10:04:09 -0700

    Resolve Tattoos from coverage visibility

--- engine/resolution/index.js
+++ engine/resolution/index.js
@@ -1,5 +1,6 @@
 import { resolveAtmosphere } from "./atmosphere.js";
 import { resolveCoverage } from "./coverage.js";
+import { resolveTattoos } from "./tattoos.js";
 
 export function resolveGeneration(selectionResult) {
   if (!selectionResult?.selections) throw new Error("Resolution requires a Selection result.");
@@ -7,6 +8,7 @@ export function resolveGeneration(selectionResult) {
   const resolvedSelections = { ...selections };
   if (selections.atmosphere) resolvedSelections.atmosphere = resolveAtmosphere(selections.atmosphere, selections.location);
   const coverage = resolveCoverage(resolvedSelections);
+  if (selections.tattoos) resolvedSelections.tattoos = resolveTattoos(selections.tattoos, coverage.tattooVisibility);
   return Object.freeze({
     selections: Object.freeze(resolvedSelections),
     randomState: selectionResult.randomState,
