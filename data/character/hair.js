351b20ab20004f17f5452b0590b69134e9306bba
Aaronchat <pughenbohm@yahoo.com>
2026-08-20 14:06:27 -0700

    Redesign Hair model for 2.1D

--- data/character/hair.js
+++ data/character/hair.js
@@ -8,14 +8,21 @@ export const CHARACTER_HAIR = Object.freeze({
       "Blue", "Cyan", "Green", "Teal", "Purple", "Magenta", "Pink", "Orange", "Pastel Pink", "Rose Gold", "Lavender",
     ]),
   }),
-  lengths: Object.freeze(["Very Long", "Long", "Medium", "Short", "Very Short", "Bald"]),
-  textures: Object.freeze(["Straight", "Wavy", "Curly"]),
+  colorTreatments: Object.freeze([
+    "Highlights", "Streaks", "Ombré", "Colored Tips", "Split Dye", "Face-Framing Color", "Contrasting Roots", "Frosted Tips",
+  ]),
   styles: Object.freeze({
-    looseDown: Object.freeze(["Layered", "Straight", "Beach Waves"]),
-    short: Object.freeze(["Bob Cut", "Pixie Cut", "French Bob", "Wolf Cut", "Shag Cut", "Lob"]),
-    ponytails: Object.freeze(["Ponytail (generic)", "High Ponytail", "Low Ponytail", "Side Ponytail"]),
-    buns: Object.freeze(["Bun (generic)", "Messy Bun", "High Bun", "Low Bun", "Space Buns"]),
+    loose: Object.freeze([
+      "Very Long Straight Hair", "Very Long Wavy Hair", "Very Long Curly Hair",
+      "Long Straight Hair", "Long Wavy Hair", "Long Curly Hair",
+      "Medium Straight Hair", "Medium Wavy Hair", "Medium Curly Hair",
+      "Short Straight Hair", "Short Wavy Hair", "Short Curly Hair",
+    ]),
+    cuts: Object.freeze(["Bob Cut", "Pixie Cut", "French Bob", "Wolf Cut", "Shag Cut", "Lob", "Mohawk", "Undercut", "Buzz Cut"]),
+    ponytails: Object.freeze(["Ponytail", "High Ponytail", "Low Ponytail", "Side Ponytail", "Twin Tails"]),
+    buns: Object.freeze(["Bun", "Messy Bun", "High Bun", "Low Bun", "Space Buns"]),
     braids: Object.freeze(["Single Braid", "French Braid", "Dutch Braid", "Fishtail Braid", "Side Braid", "Twin Braids", "Crown Braid"]),
-    alternative: Object.freeze(["Twin Tails", "Mohawk", "Undercut", "Buzz Cut"]),
+    styled: Object.freeze(["Layered Hair", "Beach Waves"]),
+    hairless: Object.freeze(["Bald"]),
   }),
 });
