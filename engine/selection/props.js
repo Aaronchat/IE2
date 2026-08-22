f6fa2de023205dc49587139f73a06e46422af875
Aaronchat <pughenbohm@yahoo.com>
2026-08-22 07:51:08 -0700

    Add Props selection domain

---
+++ engine/selection/props.js
@@ -0,0 +1,14 @@
+import { CATALOGS } from "./catalogs.js";
+import { assertMode, enforceMax, findEnabledRecord, result } from "./controls.js";
+
+export function selectProps(control) {
+  if (!control) return undefined;
+  assertMode(control, ["manual"], "Props");
+  enforceMax(control.selections, 3, "Props");
+  const values = control.selections.map(({ id, groupId }) => {
+    const record = findEnabledRecord(CATALOGS.props, id, "Prop", groupId);
+    const group = CATALOGS.props.find((entry) => entry.items.includes(record));
+    return Object.freeze({ category: group.id, record });
+  });
+  return values.length ? result("manual", Object.freeze(values)) : undefined;
+}
