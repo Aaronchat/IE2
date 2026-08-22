03b76f1086ebdbecf435560fa3e676efc07ae371
Aaronchat <pughenbohm@yahoo.com>
2026-08-21 18:41:36 -0700

    Add Longhorns Beach Party and Hippy themes

--- data/themes/genres-aesthetics.js
+++ data/themes/genres-aesthetics.js
@@ -11,5 +11,8 @@ export const THEME_GENRES_AESTHETICS = Object.freeze({
     Object.freeze({ id: "victorian", name: "Victorian", prompt: "Victorian" }),
     Object.freeze({ id: "noir", name: "Noir", prompt: "Noir" }),
     Object.freeze({ id: "psychedelic", name: "Psychedelic", prompt: "Psychedelic" }),
+    Object.freeze({ id: "longhorns", name: "Longhorns", prompt: "Longhorns" }),
+    Object.freeze({ id: "beach-party", name: "Beach Party", prompt: "Beach party" }),
+    Object.freeze({ id: "hippy", name: "Hippy", prompt: "Hippy" }),
   ]),
 });
